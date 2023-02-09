cd380a9ae781ab4d080856cd8606ce2f28e15ec3
Tiago Chacon <tiagochacon80@gmail.com>
2023-02-09 16:26:31 -0500

    Converting the request that comes in bytes via json

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-var consultaCep = fetch('http://viacep.com.br/ws/01001000/json');
+var consultaCep = fetch('http://viacep.com.br/ws/01001000/json')
+       .then(resposta => resposta.json())
+       .then(r => console.log(r));
+
+console.log(consultaCep);
 
 
 
