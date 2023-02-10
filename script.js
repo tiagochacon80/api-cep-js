a889272e346048b8b5b9a2b86276fef9fbde35aa
Tiago Chacon <tiagochacon80@gmail.com>
2023-02-10 15:06:21 -0500

    using 'catch' to catch the error

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-var consultaCep = fetch('http://viacep.com.br/ws/01001000/json')
+var consultaCep = fetch('http://viacep.com.br/ws/0100100/json')
        .then(resposta => resposta.json())
-       .then(r => console.log(r));
+       .then(r => {
+            if (r.erro) {
+                throw Error('Esse cep nao existe!')
+            } else 
+                console.log(r)
+        })            
+        .catch(erro => console.log(erro));
 
 console.log(consultaCep);
 
