ae3846db61aa3bacfd9170c4499b8a59a14caf94
Tiago Chacon <tiagochacon80@gmail.com>
2023-02-14 12:43:12 -0500

    Getting the error, with a custom message

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 async function busacEndereco() {
-    var consultaCep = await fetch('http://viacep.com.br/ws/01001000/json')    
-    var consultaCepConvertida = await consultaCep.json();
-    console.log(consultaCepConvertida);
+
+    try {
+        var consultaCep = await fetch('http://viacep.com.br/ws/01001250/json')    
+        var consultaCepConvertida = await consultaCep.json();
+        if (consultaCepConvertida) {
+            throw Error('CEP nao existente!');
+        }
+        console.log(consultaCepConvertida);
+    }
+    catch (erro) {
+        console.log(erro);
+    }
+    
 }       
 
 busacEndereco();
