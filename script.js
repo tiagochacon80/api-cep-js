90384479e9207bbcb9332cfb2e94b82e62b7c644
Tiago Chacon <tiagochacon80@gmail.com>
2023-02-14 13:06:59 -0500

    Passing parameter to the function

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-async function busacEndereco() {
-
+async function busacEndereco(cep) {
     try {
-        var consultaCep = await fetch('http://viacep.com.br/ws/01001250/json')    
+        var consultaCep = await fetch(`http://viacep.com.br/ws/${cep}/json`)    
         var consultaCepConvertida = await consultaCep.json();
         if (consultaCepConvertida) {
             throw Error('CEP nao existente!');
         }
         console.log(consultaCepConvertida);
+        return consultaCepConvertida;
     }
     catch (erro) {
         console.log(erro);
-    }
-    
-}       
+    }    
+}   
 
-busacEndereco();
+
 
 
 
