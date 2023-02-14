93e83c0ec20e4ce5715b8a7afb716668616d6e43
Tiago Chacon <tiagochacon80@gmail.com>
2023-02-14 13:41:45 -0500

    Autocomplete - calling the element with the event

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 async function busacEndereco(cep) {
     try {
-        var consultaCep = await fetch(`http://viacep.com.br/ws/${cep}/json`)    
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)    
         var consultaCepConvertida = await consultaCep.json();
-        if (consultaCepConvertida) {
+        if (consultaCepConvertida.erro) {
             throw Error('CEP nao existente!');
         }
         console.log(consultaCepConvertida);
@@ -12,6 +12,9 @@ async function busacEndereco(cep) {
         console.log(erro);
     }    
 }   
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => busacEndereco(cep.value));
 
 
 
